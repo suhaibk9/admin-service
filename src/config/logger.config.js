3bb9e5f50d930fd035bd9db90879768d1f7595c0
Suhaib Khan <suhaib0900@gmail.com>
2024-06-24 15:47:33 +0100

    Logs Pushed to Mongo

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,17 +1,57 @@
 const winston = require('winston');
-const allowedTransports = [];
-//If the environment is development then log the logs to the console
-allowedTransports.push(new winston.transports.Console());
-// allowedTransports.push(new winston.transports.File({});
+require('winston-mongodb');
+const { LOG_DB_URL } = require('./server.config');
 
+require('winston-mongodb');
+const allowedTransports = [];
+//Putting on Console
+allowedTransports.push(
+  //No levels are passed so it will log all levels
+  //No collection is passed so it will log in the default collection
+  //No db is passed so it will log in the default db
+  new winston.transports.Console({
+    //Overwriting the default
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      winston.format.printf((log) => {
+        return `${log.timestamp} [${log.level}]: ${log.message}`;
+      })
+    ),
+  })
+);
+//Putting in MongoDB
+allowedTransports.push(
+  new winston.transports.MongoDB({
+    //Logging only error level logs
+    //Log in the logs collection
+    level: 'error',
+    db: LOG_DB_URL,
+    collection: 'logs',
+    // format: winston.format.combine(
+    //   winston.format.timestamp({
+    //     format: 'YYYY-MM-DD HH:mm:ss',
+    //   }),
+    //   winston.format.printf((log) => {
+    //     return `${log.timestamp} [${log.level.toUpperCase()}]: ${log.message}`;
+    //   })
+    // ),
+  })
+);
+//Putting in File
+allowedTransports.push(
+  new winston.transports.File({
+    filename: `app.log`,
+  })
+);
 const logger = winston.createLogger({
+  //Default Format
   format: winston.format.combine(
-    //First Argument is the format of the timestamp
     winston.format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
     }),
-    //Second Argument is the format of the log message
-    //Level is the log level like info, error, warn etc
     winston.format.printf((log) => {
       return `${log.timestamp} [${log.level.toUpperCase()}]: ${log.message}`;
     })
